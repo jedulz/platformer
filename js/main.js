78d1e2e7fc1852fc863bbe704b04dd63af611868
CORPNET\jesus.quiroz <jquiroz@hscloudsuite.com>
2018-09-18 15:10:31 -0400

    fixed jumping to not bounce off walls, added constant gravity when not on ground

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var ctx = canvas.getContext("2d");
 
 //create game objects
 var player = new Player();
-var platform = new Platform(500,500,200,20,getRandomColor());
+var platform = new Platform(500, 500, 200, 20, getRandomColor());
 
 
 /*
@@ -19,32 +19,7 @@ window.addEventListener('resize', function(){
 
 });
 
-
-//player controls
-document.addEventListener("keydown", function(e){
-  // right
-  if(e.keyCode == 39){
-    player.rightPressed = true;
-  }
-  //left
-  else if(e.keyCode == 37){
-    player.leftPressed = true;
-  }
-  else if(e.keyCode == 38){
-    player.hasJumped =true;
-  }
-});
-document.addEventListener("keyup", function(e){
-  if(e.keyCode == 39) {
-    player.rightPressed = false;
-  }
-  else if(e.keyCode == 37) {
-    player.leftPressed = false;
-
-  }
-});
 this.drawGround = function(){
-
     ctx.beginPath();
     ctx.rect(0, canvas.height-20, canvas.width, canvas.height);
     ctx.fillStyle = "#0CB200";
@@ -54,24 +29,27 @@ this.drawGround = function(){
 
 //Random Color Generator
 function getRandomColor() {
-	  var letters = '0123456789ABCDEF';
-	  var color = '#';
-	  for (var i = 0; i < 6; i++) {
-	    color += letters[Math.floor(Math.random() * 16)];
-	  }
-	  return color;
-	}
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 //create game loop
 function animate(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   this.drawGround();
   player.update();
-  platform.draw();  
+  platform.draw();
   ctx.fillStyle ="#000";
   ctx.font = '30px Arial';
-
 }
 
-//run game loop
-setInterval(animate, 10);
+function init(){
+  //run game loop
+  setInterval(animate, 10);
+}
+
+init();
--- js/player.js
+++ js/player.js
@@ -1,3 +1,30 @@
+//player controls
+document.addEventListener("keydown", function(e){
+  // right
+  if(e.keyCode == 39){
+    player.rightPressed = true;
+  }
+  // left
+  else if(e.keyCode == 37){
+    player.leftPressed = true;
+  }// up
+  else if(e.keyCode == 38){
+    if(!player.hasJumped){
+      player.jump();
+    }
+  }
+});
+
+document.addEventListener("keyup", function(e){
+  if(e.keyCode == 39) {
+    player.rightPressed = false;
+  }
+  else if(e.keyCode == 37) {
+    player.leftPressed = false;
+
+  }
+});
+
 //Player Object
 function Player(){
   this.width = 100;
@@ -9,13 +36,7 @@ function Player(){
   this.leftPressed = false;
   this.rightPressed = false;
   this.hasJumped = false;
-
-
-  this.jump = function(){
-	  if(!this.hasJumped){
-		  this.hasJumped = true;
-  }
-  }
+  this.gravity = 0.15;
 
   this.draw = function(){
     ctx.beginPath();
@@ -25,49 +46,39 @@ function Player(){
     ctx.closePath();
   }
 
+  this.jump = function(){
+    this.dy = -7.8;
+    this.hasJumped = true;
+  }
+
   this.update = function(){
+    //add gravity
+    this.dy += this.gravity;
+    this.y += this.dy;// this will also add jump when jumping
 
     //Going right
-    if(this.rightPressed && this.x + this.width < canvas.width){
+    if(this.rightPressed && this.x + this.width + this.dx < canvas.width){
       this.x += this.dx;
     }
     //Going left
-    if(this.leftPressed && this.x > 0){
+    if(this.leftPressed && this.x - this.dx > 0){
       this.x -= this.dx;
     }
-    // on ground
-    //Jumping
-    if(this.hasJumped){
-    	this.y -= this.dy;
-      this.dx = 3;
-    	if(this.y <= this.height - 400||this.y <= 0){
-    		this.dy = -6;
-    	}
-    	if(this.y >= canvas.height - this.height -20){
-    		this.hasJumped = false;
-    		this.dy = 4;
-        this.dx = 7;
-    	}
 
-    }
-    
-    
-
-    // window resizing
-
-    // player y check
-    if(!this.hasJumped){
-      this.y = canvas.height - this.height-20;
+    //on ground
+    if(this.y >= canvas.height - this.height - 20){
+      this.hasJumped = false;
+      this.y = canvas.height - this.height - 20;
     }
 
     // player x check
     if(this.x + this.width > canvas.width){
       this.x = canvas.width - this.width;
-    }
+    }    
 
     ctx.fillText("X Position: " + Math.round(this.x*10)/10,100,50);
     ctx.fillText("Y Position: " + Math.round(this.y*10)/10,100,100);
     ctx.fillText("hasjumped?: " + this.hasJumped,100,150);
     this.draw();
   }
-  }
+}
